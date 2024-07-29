e7e0f1ec700a30e7a6578890511fe3d3b2105463
verozroke <73778760+verozroke@users.noreply.github.com>
2024-07-29 21:15:24 +0500

    needed modules and packages added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  formkit: {
+    // Experimental support for auto loading (see note):
+    autoImport: true,
+    configFile: './formkit.config.ts',
+  },
+  modules: ["@nuxt/image", "@formkit/nuxt"]
 })
