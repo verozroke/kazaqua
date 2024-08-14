3df50f64be47d27de087db9073022dc9fc7fe333
verozroke <73778760+verozroke@users.noreply.github.com>
2024-08-14 20:44:02 +0500

    products added

--- components/product/card.vue
+++ components/product/card.vue
@@ -2,12 +2,12 @@
   <div class="relative h-[450px] w-[300px] bg-white rounded-md shadow-xl border">
     <NuxtImg
       :src="product.image"
-      class="image w-full h-full inset-0 absolute object-center bg-white rounded-md object-cover"
+      class="w-full h-full inset-0 absolute object-center bg-white rounded-md object-cover"
     />
-    <div class="flex w-full absolute p-5 bottom-0 justify-between">
-      <div class="text-water text-2xl tracking-tight font-bold">{{ product.title }}</div>
-      <div class="text-white text-2xl tracking-wider font-bold">{{ product.price }}</div>
-
+    <div class="overlay absolute inset-0"></div>
+    <div class="flex w-full absolute p-5 bottom-0 justify-between items-end gap-5">
+      <div class="text-water text-sm tracking-tight font-bold">{{ product.title }}</div>
+      <div class="text-white text-sm w-20 h-fit tracking-wider font-bold text-right">{{ product.price }}</div>
     </div>
   </div>
 </template>
@@ -31,7 +31,7 @@ defineProps<{
 </script>
 
 <style scoped>
-.image {
+.overlay {
   background: linear-gradient(to top, rgba(0, 0, 0, 0.692) 10%, transparent 30%);
 }
 </style>
--- components/product/swiper.vue
+++ components/product/swiper.vue
@@ -34,7 +34,7 @@ import 'swiper/css';
 
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
-import { EffectCoverflow, FreeMode, Mousewheel, Scrollbar } from 'swiper/modules';
+import { EffectCoverflow, Mousewheel, Scrollbar } from 'swiper/modules';
 
 
 
@@ -43,39 +43,39 @@ const modules = ref([EffectCoverflow, Mousewheel, Scrollbar])
 const products = ref<Product[]>([
   {
     id: '1',
-    title: 'Вода 1',
-    image: '/products/bottle.webp',
-    price: '100 ₸'
+    title: 'Подставка под бутыль 19л передвижная',
+    image: '/products/1.jpg',
+    price: '4500₸'
   },
   {
     id: '2',
-    title: 'Вода 2',
-    image: '/products/bottle.webp',
-    price: '100 ₸'
+    title: 'Подставка под бутыль 19л (диспенсер)',
+    image: '/products/2.jpg',
+    price: '10000₸'
   },
   {
     id: '3',
-    title: 'Вода 3',
-    image: '/products/bottle.webp',
-    price: '100 ₸'
+    title: 'Помпа AquaWork российское производство',
+    image: '/products/3.jpg',
+    price: '3500₸'
   },
   {
     id: '4',
-    title: 'Вода 4',
-    image: '/products/bottle.webp',
-    price: '100 ₸'
+    title: 'Помпа китайская',
+    image: '/products/4.jpg',
+    price: '2000₸'
   },
   {
     id: '5',
-    title: 'Вода 5',
-    image: '/products/bottle.webp',
-    price: '100 ₸'
+    title: 'Электрическая помпа',
+    image: '/products/5.jpg',
+    price: '2500₸'
   },
   {
     id: '6',
-    title: 'Вода 6',
-    image: '/products/bottle.webp',
-    price: '100 ₸'
+    title: 'Вода 19л первая бутылка 2000, следующие (замены)',
+    image: '/products/6.jpg',
+    price: '700₸'
   },
 ])
 </script>
